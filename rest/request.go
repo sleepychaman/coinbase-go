409d31b7850081797ea61bc071cf7440aba758ba
sleepychaman <abichouryan@gmail.com>
2023-01-11 16:06:16 +0100

    initial commit

--- rest/request.go
+++ rest/request.go
@@ -63,7 +63,7 @@ func (c *Client) do(r Requester) (*fasthttp.Response, error) {
 		return nil, err
 	}
 
-	fmt.Printf("%+v\n", string(res.Body()))
+	// fmt.Printf("%+v\n", string(res.Body()))
 	// no usefull headers
 
 	if res.StatusCode() != 200 {
